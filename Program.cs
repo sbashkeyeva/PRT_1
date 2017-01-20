bae2cc7abf13af557998cfcd0f4b42948a19c792
Symbat Bashkeyeva <symbatbashkeyeva@Symbat.local>
2017-01-20 10:09:32 +0600

    Fist C

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_World
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadKey();
+		}
+	}
+}
