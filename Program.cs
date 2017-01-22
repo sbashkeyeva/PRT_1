a77109319ecf5ae7626b20aa473025cdc1e0d001
Symbat <sbashkeyeva@gmail.com>
2017-01-22 23:03:18 +0600

    Prime Number

--- Program.cs
+++ Program.cs
@@ -4,10 +4,30 @@ namespace Hello_World
 {
 	class MainClass
 	{
+		static bool isPrime (int n)
+		{
+			if (n == 1) return false;
+			if (n == 2) return true;
+			for (int i = 2; i < n - 1; i++) 
+			{
+				if (n % i == 0) return false;
+			}
+			return true; 
+		}
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			Console.ReadKey();
+			int a = args.Length;
+			for (int k = 0; k < a; k++)
+			{
+				string s = args[k];
+				int p = int.Parse(s);
+				if (isPrime(p) == true)
+				{
+					Console.WriteLine(p); 
+				}
+			}
+
+				
 		}
 	}
 }
